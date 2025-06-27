fdc6ac980eaec7ec998838eab6576ce5e93162f3
solomon <tsegasolomon635@gmail.com>
2025-06-27 10:48:14 +0300

    fix errors 5

--- src/app/(protected)/list/teachers/[id]/page.tsx
+++ src/app/(protected)/list/teachers/[id]/page.tsx
@@ -1,5 +1,4 @@
 import Announcements from "@/components/Announcements";
-// import BigCalendarContainer from "@/components/BigCalendarContainer";
 import FormContainer from "@/components/FormContainer";
 import Performance from "@/components/Performance";
 import prisma from "@/lib/prisma";
